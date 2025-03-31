350e2db17d13b001929ac18a203d294128980acb
Fabio Alves <alvesdebarrosf@gmail.com>
2025-03-30 22:25:30 -0300

    alteracao no banco

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,15 @@
 import dotenv from 'dotenv';
+import { resolve } from 'path';
+
 dotenv.config();
 
 export default {
   dialect: 'sqlite',
-  storage: './src/database/db_sistema.sqlite',
+  // Aqui usamos o resolve para garantir que o caminho seja correto
+  storage: resolve(__dirname, './src/database/db_sistema.sqlite'), // Certifique-se de que este caminho é válido
   define: {
     timestamps: true,
     underscored: true,
   },
 };
+
