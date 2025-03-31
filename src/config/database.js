3199db1c7b3ab771acf433c4e8f2825e533c5ddb
Fabio Alves <alvesdebarrosf@gmail.com>
2025-03-30 21:37:12 -0300

    3

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 
-module.exports = {
+export default {
   dialect: 'sqlite',
-  storage: `./${process.env.DATABASE_URL.replace('sqlite:', '')}`,
+  storage: './src/database/db_sistema.sqlite',
   define: {
     timestamps: true,
     underscored: true,
